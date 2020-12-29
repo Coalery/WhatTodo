fb1f6fc63f4b2234e47650e4a2e2993865820199
러리 <doralife12@naver.com>
2020-12-29 22:46:24 +0900

    Add: Conditional Widget

---
+++ app/lib/widget/conditional_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ConditionalWidget extends StatelessWidget {
+  final bool _condition;
+  final Widget _trueWidget;
+  final Widget _falseWidget;
+
+  ConditionalWidget({
+    Key key,
+    @required bool condition,
+    @required Widget trueWidget,
+    Widget falseWidget
+  })
+  : assert(condition != null),
+    assert(trueWidget != null),
+    assert(falseWidget != null),
+    this._condition = condition,
+    this._trueWidget = trueWidget,
+    this._falseWidget = falseWidget ?? SizedBox(),
+    super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return _condition ? _trueWidget : _falseWidget;
+  }
+}
